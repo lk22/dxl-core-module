159b00485eb2ed0633322246709a898cc1b48ab5
Leo Knudsen <lk22>
2022-07-13 22:23:56 +0200

    removin custom modal, using bootstrap modal + create logging method to cron specific actions

--- src/Classes/Utilities/Logger.php
+++ src/Classes/Utilities/Logger.php
@@ -18,6 +18,8 @@ if(!class_exists('Logger'))
          */
         const LOGFILE = ABSPATH . "wp-content/plugins/dxl-core/dxl.log";
 
+        const CRON_LOG_FILE = ABSPATH . "wp-content/plugins/dxl-core/dxl-cron.log";
+
         /**
          * return a new or existing instance of the logger utility
          *
@@ -43,8 +45,10 @@ if(!class_exists('Logger'))
          */
         public function log(string $message, $plugin = "dxl", $level = 1)
         {
+            global $current_user;
             $logLevel = $this->setLogLevel($level);
-            $logMessage = date("d-m-Y H:i:s", strtotime('now')) . " (" . $logLevel . ") User: " . wp_get_current_user()->user_login . ", " . $message . " (dxl-" . $plugin . ")\r";
+            
+            $logMessage = date("d-m-Y H:i:s", strtotime('now')) . " (" . $logLevel . ") User: " . ($current_user->user_login) ? $current_user->user_login : '' . ", " . $message . " (dxl-" . $plugin . ")\r";
 
             if( is_file(self::LOGFILE) && file_exists(self::LOGFILE) ) {
 	    		return file_put_contents(self::LOGFILE, $logMessage, FILE_APPEND);
@@ -52,6 +56,29 @@ if(!class_exists('Logger'))
 
 	    	touch(self::LOGFILE); 
 	    	return file_put_contents(self::LOGFILE, $logMmessage, FILE_APPEND);
+        }
+
+        /**
+         * Log the message to the logfile
+         *
+         * @param string $message
+         * @param string $plugin
+         * @param integer $level
+         * @return void
+         */
+        public function logCronReport(string $message, $level = 4)
+        {
+            global $current_user;
+            $logLevel = $this->setLogLevel($level);
+            
+            $logMessage = date("d-m-Y H:i:s", strtotime('now')) . " (" . $logLevel . ") " . $message "\r";
+
+            if( is_file(self::CRON_LOG_FILE) && file_exists(self::CRON_LOG_FILE) ) {
+	    		return file_put_contents(self::CRON_LOG_FILE, $logMessage, FILE_APPEND);
+	    	}
+
+	    	touch(self::CRON_LOG_FILE); 
+	    	return file_put_contents(self::CRON_LOG_FILE, $logMmessage, FILE_APPEND);
         }
 
         /**
@@ -70,6 +97,10 @@ if(!class_exists('Logger'))
                     break;
                 case 3: 
                     return "API";
+                    break;
+
+                case 4: 
+                    return "CRON";
                     break;
             }
         }
