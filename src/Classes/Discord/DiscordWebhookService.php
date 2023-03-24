31484d6ee47a50f6e22b4b1dee549f8a92093269
Leo Knudsen <lk22>
2023-03-24 16:23:24 +0100

    adding discord webhook service

--- src/Classes/Discord/DiscordWebhookService.php
+++ src/Classes/Discord/DiscordWebhookService.php
@@ -62,12 +62,14 @@ if ( ! class_exists('DiscordWebhookService') ) {
      * @return void
      */
     private function send($data) {
-      $curl = curl_init($this->webhook_url);
+      $curl = curl_init("localhost:3000/discord");
+      // $curl = curl_init($this->webhook_url);
       curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
       curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
       curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($curl, CURLOPT_HTTPHEADER, [
         'Content-Type: application/json',
+        'User-Agent: DXL Webhook',
         'Content-Length: ' . strlen(json_encode($data))
       ]);
 
